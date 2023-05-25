54af9c70d7385a19c25295421890c4a024615ec9
Lida Kovtunenko <kovtunenko.lida@nltu.lviv.ua>
2023-05-25 17:58:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {   //використаний SortedList
+        SortedList<int, Publication> publications = new SortedList<int, Publication>
+        {
+            { 1, new Publication("Flowers1", 2021, 300) },
+            { 2, new Publication("Book1", 2020, 250) },
+            { 3, new Publication("Book2", 2022, 50) },
+            { 4, new Publication("Flowers2", 2020, 250) }
+        };
+        //додаємо
+        publications.Add(5, new Publication("Flowers3", 2023, 200));
+
+        Console.WriteLine("\n\tSorted:");
+        foreach (KeyValuePair<int, Publication> publication in publications)
+        {
+            Console.WriteLine("Title: {0}, Year: {1}, Pages: {2}", publication.Value.Title, publication.Value.Year, publication.Value.PageAmount);
+        }
+        //видаляємо
+        publications.Remove(4);
+        //серіалізація
+        Publication.Serialize(publications, "publications.dat");
+
+        SortedList<int, Publication> deserializedPublications = Publication.Deserialize("publications.dat");
+        Console.WriteLine("\n\tDeserialized:");
+        //десеріалізація всіх об'єктів в окремому класі 
+        foreach (KeyValuePair<int, Publication> publication in deserializedPublications)
+        {
+            Console.WriteLine("Title: {0}, Year: {1}, Pages: {2}", publication.Value.Title, publication.Value.Year, publication.Value.PageAmount);
+        }
+    }
+}
---
+++ Publication.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+//є інтерфейс
+interface IComparable<T>
+{
+    int CompareTo(T other);
+}
+
+[Serializable] // є серіалізація
+class Publication : IComparable<Publication>
+{ //є поля назва, рік і к-сть сторінок, разом з властивостями 
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public int PageAmount { get; set; }
+    // і конструктором
+    public Publication(string title, int year, int pageAmount)
+    {
+        Title = title;
+        Year = year;
+        PageAmount = pageAmount;
+    }
+    
+    public int CompareTo(Publication other)
+    {
+        return Title.CompareTo(other.Title);
+    }
+    //прописані методи серіалізації і де- об'єктів
+    public static void Serialize(SortedList<int, Publication> publications, string fileName)
+    {
+        FileStream Sstream = new FileStream("publications.dat", FileMode.Create);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(Sstream, publications);
+        Sstream.Flush();
+        Sstream.Close();
+    }
+
+    public static SortedList<int,Publication> Deserialize(string fileName)
+    {
+        FileStream Dstream = new FileStream("publications.dat", FileMode.Open);
+        BinaryFormatter bf = new BinaryFormatter();
+        return (SortedList<int, Publication>)bf.Deserialize(Dstream);
+    }
+
+
+    //нижче є набагато гнучкіший варіант реалізація серілазіції і де-. Зверху надто жорстке але хай буде 
+
+    //public static void Serialize(List<Publication> publications, string fileName)
+    //{
+    //    BinaryFormatter formatter = new BinaryFormatter();
+    //    using (FileStream stream = new FileStream(fileName, FileMode.Create))
+    //    {
+    //        formatter.Serialize(stream, publications);
+    //    }
+    //}
+
+    //public static List<Publication> Deserialize(string fileName)
+    //{
+    //    BinaryFormatter formatter = new BinaryFormatter();
+    //    using (FileStream stream = new FileStream(fileName, FileMode.Open)) 
+    //    {
+    //        return (List<Publication>)formatter.Deserialize(stream);
+    //    }
+    //}
+}
